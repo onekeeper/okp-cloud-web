3533972cd5bea788bd32fb4976304fd0ae04a5ca
wein <wein@hzmc.com.cn>
2017-07-12 16:39:05 +0800

    日期过滤器修改

--- app/src/js/directives/dateFilter.js
+++ app/src/js/directives/dateFilter.js
@@ -12,8 +12,30 @@
  angular.module('myappApp')
  .filter('dateFilter', function(){
  	return function(time){
- 		var bjTime = new Date(time)
- 		var res = bjTime.getFullYear() + '-' + (bjTime.getMonth() + 1) + '-' + bjTime.getDate() + ' ' + bjTime.getHours() + ':' + bjTime.getMinutes() + ':' + bjTime.getSeconds(); 
- 		return res 
+ 		var date = new Date(time);
+ 		var symbol1 = "-";
+ 		var symbol2 = ":";
+ 		var month = date.getMonth() + 1;
+ 		var day = date.getDate();
+ 		var hour = date.getHours();
+ 		var minute= date.getMinutes();
+ 		var second = date.getSeconds()
+ 		if (month >= 1 && month <= 9) {
+ 			month = "0" + month;
+ 		}
+ 		if (day >= 0 && day <= 9) {
+ 			day = "0" + day;
+ 		}
+ 		if (hour >= 1 && hour <= 9) {
+ 			hour = "0" + hour;
+ 		}
+ 		if (minute >= 0 && minute <= 9) {
+ 			minute = "0" + minute;
+ 		}
+ 		if (second >= 0 && second <= 9) {
+ 			second = "0" + second;
+ 		}
+ 		var res = date.getFullYear() + symbol1 + month + symbol1 + day + " " + hour + symbol2 + minute + symbol2 + second;
+ 		return res; 
  	}
  })
